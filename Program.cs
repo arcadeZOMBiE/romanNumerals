cc03123d62a749ba0d4c2dca16477ecbbbd1bf6a
arcadeZOMBiE <mark@mlambert.co.uk>
2022-03-22 16:33:31 +0000

    Initial Version
    Initial version

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+using RomanNumerals;
+
+string passedValue = args.Length > 0 ? args[0].ToString() : "";
+
+if (passedValue == "")
+{
+    Console.WriteLine("Please enter an integer or Roman Numeral to convert:");
+
+    var response = Console.ReadLine();
+    if (response != null) passedValue = response;
+}
+
+if (passedValue == "")
+{
+    Console.WriteLine("Nothing to convert!");
+    return;
+}
+
+try
+{
+    int value = int.Parse(passedValue);
+
+    var rnc = new RomanNumeralConverter();
+    Console.WriteLine(rnc.ConvertToRomanNumerals(value));
+
+} catch
+{
+    var rnc = new RomanNumeralConverter();
+    Console.WriteLine(rnc.ConvertToInteger(passedValue));
+}
---
+++ RomanNumeralConverter.cs
@@ -0,0 +1,64 @@
+﻿using System.Text;
+
+namespace RomanNumerals
+{
+    public class RomanNumeralConverter
+    {
+        private readonly Dictionary<int, string> keyValues = new()
+        {
+            {1000,"M"},
+            {900,"CM"},
+            {500,"D"},
+            {400,"CD"},
+            {100,"C"},
+            {90,"XC"},
+            {50,"L"},
+            {40,"XL"},
+            {10,"X"},
+            {9,"IX"},
+            {5,"V"},
+            {4,"IV"},
+            {1,"I"}
+        };
+
+        public string ConvertToRomanNumerals(int value)
+        {
+            StringBuilder returnValue = new();
+
+            foreach (var key in keyValues)
+            {
+                var count = value / key.Key;
+                for(int i = 0; i < count; i++)
+                {
+                    returnValue.Append(key.Value);
+                }
+                value %= key.Key;
+            }
+
+            return returnValue.ToString();
+        }
+
+        public int ConvertToInteger(string RomanNumerals)
+        {
+            var returnValue = 0;
+          
+            for(var i = 2; i > 0; i--)
+            {
+                foreach (var key in keyValues)
+                {
+                    if (key.Value.Length == i)
+                    {
+                        int pos = 0;
+                        while ((pos = RomanNumerals.IndexOf(key.Value)) != -1)
+                        {
+                            returnValue += key.Key;
+                            RomanNumerals = RomanNumerals.Remove(pos, i);
+                        }
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}
